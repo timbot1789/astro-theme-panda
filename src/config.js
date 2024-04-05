f71be178e862c695a9639bac9ceb6d89d73af1b8
Tim Standen <timmstanden@gmail.com>
2024-04-04 21:04:46 -0700

    first commit: remove default filler

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PandaConfig = {
-    title: 'Panda Blog',
-    description: 'Panda Blog, a blog powered by Astro',
+    title: 'Timbot',
+    description: 'A Caffeinated Coder',
     start: '2016',
     site: 'https://astro-theme-panda.vercel.app/',
     defaultLocale: 'en',
@@ -10,7 +10,6 @@ export const PandaConfig = {
     ],
     footer: [
         { title: 'rss', url: '/rss.xml/' },
-        { title: 'contact', url: 'https://github.com/yuhangch/astro-theme-panda/issues/new' },
-        { title: 'github', url: 'https://github.com/yuhangch/astro-theme-panda' }
+        { title: 'github', url: 'https://github.com/timbot1789' }
     ]
 }
